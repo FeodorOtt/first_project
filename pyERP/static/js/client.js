bc2a291fffb2adbb473f4943c6be3f3e94542646
Vladyslav Bukreiev <bukerman@gmail.com>
2019-01-29 05:03:03 +0200

    12

--- pyERP/static/js/client.js
+++ pyERP/static/js/client.js
@@ -2,36 +2,28 @@ $(function(){
 
   var dictionary = {
     'en': {
-      'db_client_id': 'Dt Client',
-      'cr_client_id': 'Ct Client',
-      'amount': 'Amount',
-      'currency_id': 'Currency',
-      'amount_e': 'Equivalent',
-      'payment_details': 'Details'
+      'name': 'Name',
+      'type': 'Type',
+      'is_resident': 'Resident',
+      'responsible_client': 'Responsible'
     },
     'de': {
-      'db_client_id': 'Dt Client',
-      'cr_client_id': 'Ct Client',
-      'amount': 'Betrag',
-      'currency_id': 'Währung',
-      'amount_e': 'gleichwertig',
-      'payment_details': 'Zahlungszweck'
+      'name': 'Name',
+      'type': 'Type',
+      'is_resident': 'Resident',
+      'responsible_client': 'Responsible'
     },
     'ru': {
-      'db_client_id': 'Клиент Дт',
-      'cr_client_id': 'Клиент Кт',
-      'amount': 'Сумма',
-      'currency_id': 'Валюта',
-      'amount_e': 'Эквивалент',
-      'payment_details': 'Примечание'
+      'name': 'Имя',
+      'type': 'Тип',
+      'is_resident': 'Резидент',
+      'responsible_client': 'Ответственный'
     },
     'ua': {
-      'db_client_id': 'Клієнт Дт',
-      'cr_client_id': 'Клієнт Кт',
-      'amount': 'Сума',
-      'currency_id': 'Валюта',
-      'amount_e': 'Еквивалент',
-      'payment_details': 'Призначення'
+      'name': "Ім'я",
+      'type': 'Тип',
+      'is_resident': 'Резидент',
+      'responsible_client': 'Відповідальний'
     }
   };
 
@@ -39,36 +31,25 @@ $(function(){
 
   var formatMessage = DevExpress.localization.formatMessage;
 
-  var transaction = new DevExpress.data.DataSource()
-  transaction = 'api/'
+  var client = new DevExpress.data.DataSource()
+  client = 'api/'
 
-  var client = {
+  var client_lu = {
     store: new DevExpress.data.CustomStore({
       key: "id",
       loadMode: "raw",
       load: function() {
-      return $.getJSON('client/api/');
+      return $.getJSON('api/');
       }
     }),
     sort: "name"
   }
 
- var currency = {
-     store: new DevExpress.data.CustomStore({
-         key: "id",
-         loadMode: "raw",
-         load: function() {
-             return $.getJSON('currency/api/');
-         }
-     }),
-     sort: "ISO_char"
- }
-
- $("#gridContainer").dxDataGrid({
-      dataSource: transaction,
+$("#gridContainer").dxDataGrid({
+      dataSource: client,
       export: {
           enabled: true,
-          fileName: "Transactions",
+          fileName: "Clients",
           allowExportSelectedData: true
       },
       allowColumnResizing: true,
@@ -92,53 +73,24 @@ $(function(){
           mode: "select"
       },
       columns: [{
-                  dataField: "db_client_id",
-                  caption: formatMessage("db_client_id"),
-                  width: 125,
+                  dataField: "name",
+                  caption: formatMessage("name"),
+                  width: 125
+                }, {
+                  dataField: "type",
+                  caption: formatMessage("type")
+                }, {
+                  dataField: "is_resident",
+                  caption: formatMessage("is_resident"),
+                }, {
+                  dataField: "responsible_client",
+                  caption: formatMessage("responsible_client"),
                   lookup: {
-                    dataSource: client,
+                    dataSource: client_lu,
                     displayExpr: "name",
                     valueExpr: "id"
                   }
-                },
-                {
-                  dataField: "cr_client_id",
-                  caption: formatMessage("cr_client_id"),
-                  width: 125,
-                  lookup: {
-                      dataSource: client,
-                      displayExpr: "name",
-                      valueExpr: "id"
-                    }
-                  },
-                {
-                  dataField: "amount",
-                  caption: formatMessage("amount"),
-                  alignment: 'right',
-                  dataType: "number",
-                  format: "#,##0.00"
-                },
-                {
-                  dataField: "currency_id",
-                  caption: formatMessage("currency_id"),
-                  lookup: {
-                    dataSource: currency,
-                    displayExpr: "ISO_char",
-                    valueExpr: "id"
-                  }
-                },
-                {
-                  dataField: "amount_e",
-                  caption: formatMessage("amount_e"),
-                  alignment: 'right',
-                  dataType: "number",
-                  format: "#,##0.00"
-                },
-                {
-                  dataField: "payment_details",
-                  caption: formatMessage("payment_details"),
-                }
-               ],
+                }],
       editing: {
           allowAdding: true,
           allowUpdating: true,
@@ -157,59 +109,20 @@ $(function(){
           allowSearch: true
       },
       groupPanel: {
-          visible: true,
+          visible: false,
       },
       scrolling: {
           mode: "virtual"
       },
-      // height: 600,
       showBorders: true,
       selection: {
           mode: "multiple",
           allowSelectAll: true
       },
-      grouping: {
+      // grouping: {
           // autoExpandAll: false,
-          expandMode: 'rowClick',
-          contextMenuEnabled: true,
-      },
-      summary: {
-          totalItems: [{
-                  column: "amount",
-                  summaryType: "sum",
-                  valueFormat: "#,##0.00",
-                  displayFormat: "{0}"
-              }, {
-                  column: "amount_e",
-                  summaryType: "sum",
-                  valueFormat: "#,##0.00",
-                  displayFormat: "{0}"
-              }, {
-                  column: "payment_details",
-                  summaryType: "count",
-                  // displayFormat: "Всего: {0}"
-              }
-            ],
-          groupItems: [{
-                  column: "amount",
-                  summaryType: "sum",
-                  valueFormat: "#,##0.00",
-                  displayFormat: "{0}",
-                  alignByColumn: true
-              }, {
-                  column: "amount_e",
-                  summaryType: "sum",
-                  valueFormat: "#,##0.00",
-                  displayFormat: "{0}",
-                  alignByColumn: true
-                },
-               {
-                  column: "payment_details",
-                  summaryType: "count",
-                  // displayFormat: "Всего: {0}",
-                  alignByColumn: true
-              }
-          ]
-      }
+      //     expandMode: 'rowClick',
+      //     contextMenuEnabled: true,
+      // },
   });
 });
